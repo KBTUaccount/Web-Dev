5ff97d76b85e38ac393dfd78ce4b5ac6d236b9d1
Pocrov4chan <anikeev63.da@gmail.com>
2025-04-04 18:08:24 +0500

    lab 9

---
+++ lab9/hh_back/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from api.models import Vacancy, Company
+
+
+class VacancySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = ['name', 'description', 'salary', 'company']
+
+
+class CompanySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['name', 'description', 'city', 'address']
